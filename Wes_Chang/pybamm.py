64f9ece2549e8e9bd7994cf6e33f0e852413d167
changbatterygroup <wesleychang1463@gmail.com>
2024-08-08 14:26:41 -0400

    update

--- Wes_Chang/pybamm.py
+++
@@ -1,7 +0,0 @@
-#pip install "pybamm[plot,cite]" -q    # install PyBaMM if it is not installed
-import pybamm
-import matplotlib.pyplot as plt
-
-model_dfn = pybamm.lithium_ion.DFN()
-sim_dfn = pybamm.Simulation(model_dfn)
-sim_dfn.solve([0, 3600])
